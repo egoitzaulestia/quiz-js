4d2c112b0d34008fb1d745368da10c4d72d38a9a
egoitzaulestia <egoitzaulestia@gmail.com>
2025-05-02 15:41:03 +0200

    little changes

---
+++ js/questions.js
@@ -0,0 +1,19 @@
+// TODO: History 2
+// Mostrar 1ª pregunta
+// question.html
+// - Pintar la pregunta 1 desde un array local.
+// - Marcar la opción elegida (clase CSS).
+
+const question = {
+  response_code: 0,
+  results: [
+    {
+      type: 'multiple',
+      difficulty: 'medium',
+      category: 'Politics',
+      question: 'What year did Gerald Ford Become President?',
+      correct_answer: '1974',
+      incorrect_answers: ['1977', '1973', '1969'],
+    },
+  ],
+};
