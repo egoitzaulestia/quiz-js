478802773e4b1b90607517f3d1e8008ef77e3679
Ainhoa Colorado <ainhoa.colorado.art@gmail.com>
2025-05-03 15:13:41 +0200

    input userName en home y guardado de valor en local storage

--- js/home.js
+++ js/home.js
@@ -1,9 +1,19 @@
-// Recuperamos el botón de la tarjeta de la página Home
+// // Recuperamos el id del formulario donde el usuario mete su nombre y le añadimos un evento para prevenir fallos. Luego cogemos el input que se encuentra dentro del formulario con "playerForm.querySelector('input')" y recogemos el valor introducido y lo asignamos a la variable playerName. Después nos aseguramos de que si el playerName esta vacío (!), haga return para que el usuario rellene el campo y si el campo está rellenado guardamos el valor "playerName" en el local storage para despues redirigir al usuario a questions.html.
 
-const startGameBtn = document.getElementById('startGameBtn');
+const playerForm = document.getElementById('playerForm');
+playerForm.addEventListener('submit', (event) => {
+  event.preventDefault();
 
-// Hacemos que al clicar en el botón nos lleve a la página de las preguntas
+  const input = playerForm.querySelector('input');
+  const playerName = input.value.trim();
 
-startGameBtn.addEventListener('click', () => {
+  if (!playerName) {
+    return;
+  }
+
+  // Guardar el nombre en localStorage
+  localStorage.setItem('nombreUsuario', playerName);
+
+  // Redirigir a la siguiente página del juego
   document.location.href = '/questions.html';
 });
